96b7829458a946a86e980253b77140108eea9780
Tudolin <angellnadalin@gmail.com>
2026-04-19 12:47:59 -0300

    fix

---
+++ src/main.tsx
@@ -0,0 +1,17 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import RoadmapPage from './routes/index'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <RoadmapPage />,
+  },
+])
+
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <RouterProvider router={router} />
+  </StrictMode>
+)
